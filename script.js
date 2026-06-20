1cd31b71c1233d327c11a7857aa9510c8a9ecce2
divyakannan-18 <divyakannan9655@gmail.com>
2026-06-20 15:08:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showWelcome() {
+    alert("Welcome to Divya's Portfolio 👩‍💻");
+}
+
+function showContact() {
+    document.getElementById("contact-msg").innerHTML =
+        "Thank you for visiting my portfolio!";
+}
